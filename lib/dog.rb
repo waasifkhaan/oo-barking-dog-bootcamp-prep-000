4a9b8cfd03b14b0513043537e532dfefa83dea7a
Wasif Khan <waasif.khaan@gmail.com>
2018-04-25 03:36:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog 
+
+def initialize(name,bark)
+  @name = name
+  @bark = bark 
+  
+end
+
+def name=(name)
+@name = name
+end
+
+def name 
+@name
+end
+
+def bark
+  puts"
+
+end
